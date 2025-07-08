1d27df36bc4addaaf28ccb1f4c9df774f185877b
Adilya <adilya.kadyrbekova.00@gmail.com>
2025-07-09 00:42:37 +0600

    save on localstorage

--- src/hook/useAdd.jsx
+++ src/hook/useAdd.jsx
@@ -3,9 +3,14 @@ import { useEffect, useState } from "react";
 const useAdd = () => {
   const [shortName, setShortName] = useState("");
   const [name, setName] = useState("");
-  const [faculties, setFaculties] = useState([]);
+  const [faculties, setFaculties] = useState(() => {
+    const saved = localStorage.getItem("faculties");
+    return saved ? JSON.parse(saved) : [];
+  });
+
   useEffect(() => {
     console.log(faculties);
+    localStorage.setItem("faculties", JSON.stringify(faculties));
   }, [faculties]);
 
   const addFaculty = (e) => {
@@ -21,27 +26,6 @@ const useAdd = () => {
     setFaculties((prev) => [...prev, newFaculty]);
     setShortName("");
     setName("");
-
-    //   fetch(data, {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       data,
-    //     })
-    //       .then((res) => res.json())
-    //       .then((data) => {
-    //         console.log(data);
-    //         setShortName((prev) => [...prev, data]);
-    //         setName((prev) => [...prev, data]);
-    //         setShortName("");
-    //         setName("");
-    //       })
-    //       .catch((err) => {
-    //         console.log(err);
-    //       }),
-    //   });
   };
 
   return {
